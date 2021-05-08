47948bcd1e48757bece6d0e731b90e6656b019d8
J Charles de Vera <jcharlesdevera@js-macbook-pro.home>
2021-05-07 22:20:22 -0700

    SQL DB Queried

---
+++ server/Router/api.js
@@ -0,0 +1,32 @@
+const express =  require('express');
+const sleepController = require('../controllers/sleepController')
+const router = express.Router();
+
+router.get('/',
+    sleepController.getUserData,
+    (req, res) => res.status(200).send(res.locals.users)
+);
+
+
+
+router.get('/users',
+    sleepController.getUserData,
+    (req, res) => res.status(200).json({})
+);
+
+router.post('',
+    sleepController.createSleepEntry,
+    (req, res) => res.status(200).json({})
+);
+
+router.patch('',
+    sleepController.updateSleepEntry,
+    (req, res) => res.status(200).json({})
+);
+
+router.delete('',
+    sleepController.deleteSleepEntry,
+    (res, req) => res.status(200).json({})
+);
+
+module.exports = router;
