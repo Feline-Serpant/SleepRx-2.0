a76e2d6cf5f63d66645beeb17234e1c4e2fc79fd
Carlos Botero-Vargas <80601385+Carlos-BoteroVargas@users.noreply.github.com>
2021-05-07 21:42:22 -0500

    backend completed, webpacked installed, dependencies updated.

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+    entry: './client/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    mode: process.env.NODE_ENV,    
+    module: {
+      rules: [
+        {
+          test: /\.jsx?/,
+          exclude: /node_modules/,
+          use: {
+              loader: 'babel-loader', // 'file-loader'
+              options: {
+                  presets: ['@babel/preset-env', '@babel/preset-react']
+              }
+          },
+      },
+      {
+          test: /\.s[ac]ss$/i,
+          exclude: /node_modules/,
+          use: [
+              'style-loader',
+              'css-loader',
+              'sass-loader'
+          ],
+        },
+      ],
+    },
+    devServer : {
+      publicPath: '/build',
+      proxy: {
+        '/api': 'http://localhost:3000'
+      },
+      hot: true, 
+    },
+};
