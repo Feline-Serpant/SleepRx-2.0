d7683ae9e9a6e487bd3cffcc2c46aa81a34d934a
Kall_MeKami <hopefelixn@gmail.com>
2021-05-08 15:21:21 -0400

    Windows Friendly Patch

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,34 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './client/src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
-    },
-    mode: process.env.NODE_ENV,    
-    module: {
-      rules: [
-        {
-          test: /\.jsx?/,
-          exclude: /node_modules/,
-          use: {
-              loader: 'babel-loader', // 'file-loader'
-              options: {
-                  presets: ['@babel/preset-env', '@babel/preset-react']
-              }
+  entry: "./client/src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader", // 'file-loader'
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
           },
+        },
       },
       {
         test: /\.s[ac]ss$/i,
         exclude: /node_modules/,
-        use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader'
-        ],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(png|jpg|gif)$/i,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 8192,
             },
@@ -43,23 +39,23 @@ module.exports = {
         test: /\.svg$/,
         use: [
           {
-            loader: 'svg-url-loader',
+            loader: "svg-url-loader",
             options: {
               limit: 10000,
             },
           },
         ],
-      }
-      ],
-    },
-    devServer: {
-      publicPath: '/build',
-      proxy: {
-        '/api': 'http://localhost:3000',
-        '/': 'http://localhost:3000'
       },
-      hot: true,
-      historyApiFallback: true,
-
+    ],
+  },
+  devServer: {
+    publicPath: "/build",
+    proxy: {
+      "/api": "http://localhost:3000",
+      "/": "http://localhost:3000",
     },
+
+    hot: true,
+    historyApiFallback: true,
+  },
 };
