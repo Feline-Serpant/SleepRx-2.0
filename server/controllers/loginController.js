14cc7b43cd9d8d866fd26c85864cd3d32714ba6b
kmaccoy <80644013+kmaccoy@users.noreply.github.com>
2021-05-09 20:37:14 -0400

    Login and Register controllers and routes set uppp

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -6,12 +6,36 @@ const loginControllers = {
     try {
       //returned data
       const values = Object.values(req.body);
+      const check  = await db.query('SELECT * FROM user_data WHERE username = ($1)', [req.body.username])
+      if(check.rowCount === 0){
       const result = await db.query('INSERT INTO public.user_data(first_name, last_name, username, password) VALUES ($1, $2, $3, $4)', values)
       //figure out how to manipulate res from db
       res.locals.users = result.rows; 
       return next();
+      } else {
+      res.locals.users = check.rows
+      return next()
+      }
 
     }catch (err){
+      console.log(err);
+      return next(err);
+    }
+  },
+
+  async loginUser (req, res, next) {
+      try{ 
+        const loginValues = Object.values(req.body);
+        const loginCheck = await db.query('SELECT * FROM user_data WHERE username = ($1) AND password = ($2)', loginValues)
+        //IF the query finds
+        if(loginCheck.rowCount){
+          return next();
+        } else {
+          //test fails, no user found
+          res.locals.err = 'Login incorrect username / password.'
+          res.status(200).redirect('/login')
+        }
+      }catch (err){
       console.log(err);
       return next(err);
     }
