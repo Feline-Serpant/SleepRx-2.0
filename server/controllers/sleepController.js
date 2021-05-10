e619a520b6d3656c1ecc68988b4a9a94979c3358
J Charles de Vera <jcharlesdevera@js-macbook-pro.home>
2021-05-09 20:17:39 -0700

    score calculator

--- server/controllers/sleepController.js
+++ server/controllers/sleepController.js
@@ -1,5 +1,58 @@
 const db = require('../models/sleepModels');
 
+const removeSpecialChars = (input) => {
+  return input.replace(/[^a-zA-Z0-9]/g, '')
+}
+const convertNumber = (object) => {
+  for(const key in object){
+    if(key === "wake_time" || key === "bed_time"){
+      object[key] = removeSpecialChars(object[key])
+    }
+    if(key != "date") object[key] = Number(object[key])
+  }
+  return object;
+}
+
+const calcScore = (object) =>{
+  const {hours_slept, exercise_time, caffeine_intake, calorie_intake, mood} = object;
+  const evalScore = sleepScore(hours_slept)+ exerciseScore(exercise_time) + caffeineScore(caffeine_intake) + calorieScore(calorie_intake) +moodScore(mood);
+  return evalScore; 
+}
+const sleepScore = (num) =>{
+  if(num<2) return -2
+  if(num<3) return -1;
+  if(num<4) return 1;
+  if(num<5) return 2;
+  if(num<7) return 3;
+  if(num>9) return 3;
+  else return 4;
+}
+console.log(sleepScore(3))
+//=ifs(E3<0.25, 0, E3<0.5, 0.5, E3<1, 1, E3>=1, 2)
+const exerciseScore = (num) =>{
+  if(num<0.25) return 0;
+  if(num<0.5) return 0.5;
+  if(num<1) return 1;
+  else return 2;
+}
+//=IFS(G3= 0, 1, G3>0, 1- G3*(0.25))
+const caffeineScore = (num) =>{
+  if(num===0) return 1;
+  if(num>0) return 1-G3(0.25);
+}
+
+const calorieScore = (num) => {
+  if(num===2000) return 1;
+  if(num<1250) return 0.5
+  if(num<1000) return 0
+}
+
+const moodScore = (num) => {
+  if(num===3) return 2;
+  if(num===2) return 1.3;
+  if(num===1) return 0.6;
+}
+
 const sleepControllers = {
   //GET REQUEST for user information on their sleep profile/data
   getUserData: async (req, res, next) =>{
@@ -37,13 +90,15 @@ const sleepControllers = {
   //POST REQUEST to create new DB entries of sleep data per user
   createSleepEntry: async (req, res, next) => {
     try{
-      const {userid, bedtime, waketime, hours_slept, exercise_time, caffeine_intake, calorie_intake, mood, score, date} = req.body;
-      const query = "INSERT INTO sleep (userid, bedtime, waketime, hours_slept, exercise_time, caffeine_intake, calorie_intake, mood, score, date) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)"
-      const values = [userid, bedtime, waketime, hours_slept, exercise_time, caffeine_intake, calorie_intake, mood, score, date]
-
+      req.body.values = convertNumber(req.body.values)
+      req.body.values.score = calcScore(req.body.values)
+      const {userid, bed_time, wake_time, hours_slept, exercise_time, caffeine_intake, calorie_intake, mood, score, date} = req.body.values;
+      const query = "INSERT INTO sleep (userid, bed_time, wake_time, hours_slept, exercise_time, caffeine_intake, calorie_intake, mood, score, date) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)"
+      const values = [userid, bed_time, wake_time, hours_slept, exercise_time, caffeine_intake, calorie_intake, mood, score, date]
+      console.log(req.body.values)
       const result = await db.query(query, values);
 
-      console.log(result);
+      // console.log(result);
 
       return next();
     } catch (err){
@@ -55,8 +110,10 @@ const sleepControllers = {
   //PATCH REQUEST to update current sleep data before it 
   updateSleepEntry: async (req, res, next) =>{
     try {
+      req.body.values = convertNumber(req.body.values)
       const {userid, sleepid} = req.params;
-      const {bed_time, wake_time, hours_slept, exercise_time, caffeine_intake, calorie_intake, mood, score, date} = req.body;
+      req.body.values.score = calcScore(req.body.values)
+      const {bed_time, wake_time, hours_slept, exercise_time, caffeine_intake, calorie_intake, mood, score, date} = req.body.values;
       const query = "UPDATE sleep SET bed_time = ($1), wake_time = ($2), hours_slept = ($3), exercise_time = ($4), caffeine_intake = ($5), calorie_intake = ($6), mood = ($7), score = ($8), date = ($9) WHERE userid = ($10) AND sleepid = ($11) RETURNING*"
       const value = [bed_time, wake_time, hours_slept, exercise_time, caffeine_intake, calorie_intake, mood, score, date, userid, sleepid]
 
